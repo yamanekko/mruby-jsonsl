9e321cf52bf36ad8a8fc3f05dea7f96d10452bf0
takahashim <maki@rubycolor.org>
2015-06-22 19:06:16 +0900

    fix test

--- test/jsonsl.rb
+++ test/jsonsl.rb
@@ -2,5 +2,5 @@ assert('JSONSL.new') do
   assert_equal(JSONSL, JSONSL.new.class)
 end
 assert('JSONSL.parse') do
-  assert_equal(true, JSONSL.new.parse('{"foo":true}'))
+  assert_equal({"foo"=>true}, JSONSL.new.parse('{"foo":true}'))
 end
