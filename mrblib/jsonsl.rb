41b97262f6c8faf2e1bb10bc3ddedc57ed3ca7d0
takahashim <maki@rubycolor.org>
2015-06-25 00:34:40 +0900

    JSONSL.parseを追加、testも追加

---
+++ mrblib/jsonsl.rb
@@ -0,0 +1,5 @@
+class JSONSL
+  def self.parse(str)
+    new.parse(str)
+  end
+end
--- test/jsonsl.rb
+++ test/jsonsl.rb
@@ -1,6 +1,25 @@
 assert('JSONSL.new') do
   assert_equal(JSONSL, JSONSL.new.class)
 end
-assert('JSONSL.parse') do
+assert('JSONSL#parse null array') do
+  assert_equal([], JSONSL.new.parse('[]'))
+end
+assert('JSONSL#parse null hash') do
+  assert_equal({}, JSONSL.new.parse('{}'))
+end
+assert('JSONSL#parse') do
   assert_equal({"foo"=>true}, JSONSL.new.parse('{"foo":true}'))
+end
+assert('JSONSL#parse2') do
+  assert_equal({"foo"=>["bar","buz",false,nil,"hoge",{"a"=>"b"}]}, JSONSL.new.parse('{"foo":["bar","buz",false,null,"hoge",{"a":"b"}]}'))
+end
+assert('JSONSL#parse3') do
+  assert_equal({"foo"=>[1,2,3.14,"hoge",{"a"=>"b"}]}, JSONSL.new.parse('{"foo":[1,2,3.14,"hoge",{"a":"b"}]}'))
+end
+assert('JSONSL.parse') do
+  assert_equal({"foo"=>true}, JSONSL.parse('{"foo":true}'))
+end
+assert('JSONSL#dup') do
+  json = JSONSL.new.dup.parse('{"foo":[1,2,3.14,"hoge",{"a":"b"}]}')
+  assert_equal(json, json.dup)
 end
